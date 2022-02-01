2048208f34c3083af99b3dc34bbc8b6918cdc266
Artur Stahov <vtemy.at.ua@gmail.com>
2022-02-01 17:16:31 +0200

    add base components Container and composition api example

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './i18n/config';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 
+import Container from './components/Container'
+
 function App() {
   const { t } = useTranslation();
 
@@ -14,11 +16,15 @@ function App() {
 
   return (
     <div className="App">
+      <Container title={ t('title_1') }>
+        <>
+          <button onClick={() => changeLanguage('ru')}>ru leng</button>
+          <button onClick={() => changeLanguage('en')}>en leng</button>
+          <button onClick={() => changeLanguage('ua')}>ua leng</button>
+        </>
+      </Container>
      <p>{ t('title_1') }</p>
 
-      <button onClick={() => changeLanguage('ru')}>ru leng</button>
-      <button onClick={() => changeLanguage('en')}>en leng</button>
-      <button onClick={() => changeLanguage('ua')}>ua leng</button>
     </div>
   );
 }
---
+++ src/components/Container/StyledComponent.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  font-size: 2.6rem;
+  font-family: 'Bangers';
+  color: #ffaa00;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+export { Wrapper, Title };
