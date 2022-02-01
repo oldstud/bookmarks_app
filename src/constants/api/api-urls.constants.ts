8da23368bba899c008cfb61a13aeaf0a40e7ebb4
Artur Stahov <vtemy.at.ua@gmail.com>
2022-02-01 12:07:38 +0200

    add constants, interfase, service auth

---
+++ src/constants/api/api-urls.constants.ts
@@ -0,0 +1,10 @@
+ enum API_URLS {
+    login = '/api/users/login',
+    reg = '/api/users/registration',
+    logout = '/api/users/logout',
+    getList = '/api/bookmarks/all',
+    getMarksItem = '/api/bookmarks',
+    updateMarksItem = '/api/bookmarks',
+}
+
+export default API_URLS;
