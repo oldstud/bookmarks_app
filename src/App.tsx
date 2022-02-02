0bf370fe24f69a98fee6cd935fe482131c147bc5
Dima <respect4me@ukr.net>
2022-02-02 21:35:35 +0200

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import './i18n/config';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 
 import Container from './components/Container'
+
 
 function App() {
   const { t } = useTranslation();
@@ -22,10 +23,55 @@ function App() {
           <button onClick={() => changeLanguage('ua')}>ua leng</button>
         </>
       </Container>
+
      <p>{ t('title_1') }</p>
 
+     <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={< Error/>} />
+      </Routes>
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h2>Artur and Dima </h2>
+    </div>
+  );
+}
+
+function Error() {
+  return (
+    <div>
+      <p>404</p>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </div>
+  );
+}
+
+
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import './Base.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
