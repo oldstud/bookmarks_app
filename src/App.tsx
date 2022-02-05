dc72de8c3f4d4939d5ab23e5b11b2b15930cd2fe
Dima <respect4me@ukr.net>
2022-02-05 14:41:08 +0200

    add nav mui

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 
 import Container from './components/Container'
+import { Navigation } from './components/Navigation';
+
 
 
 function App() {
@@ -14,8 +16,13 @@ function App() {
     i18next.changeLanguage(lng);
   }
 
+
+  
+
   return (
     <div className="App">
+       <Navigation navButtonTitle = 'Menu'/>
+
       <Container title={ t('title_1') }>
         <>
           <button onClick={() => changeLanguage('ru')}>ru leng</button>
@@ -26,51 +33,10 @@ function App() {
 
      <p>{ t('title_1') }</p>
 
-     <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-  </nav>
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={< Error/>} />
-      </Routes>
+  
     </div>
   );
 }
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-function About() {
-  return (
-    <div>
-      <h2>Artur and Dima </h2>
-    </div>
-  );
-}
-
-function Error() {
-  return (
-    <div>
-      <p>404</p>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </div>
-  );
-}
-
 
 
 
---
+++ src/pages/template/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Page:React.FC = () => {
+    return (
+        <div>
+            <p>TEMPLATE PAGE</p>
+            <p>TEMPLATE PAGE</p>
+            <p>TEMPLATE PAGE</p>
+            <p>TEMPLATE PAGE</p>
+            <p>TEMPLATE PAGE</p>
+        </div>
+    )
+}
