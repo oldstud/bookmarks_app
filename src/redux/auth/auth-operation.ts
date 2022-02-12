5ef15115b40749795cbf507728671bb837b1c41a
Artur Stahov <vtemy.at.ua@gmail.com>
2022-02-12 16:34:23 +0200

    add base auth store, private/public routing and Auth page component

---
+++ src/redux/auth/auth-operation.ts
@@ -0,0 +1,75 @@
+import * as action from './auth-actions';
+import axios from 'axios';
+import {
+    httpLogin,
+    httpRegistration,
+    httpLogOut,
+} from '../../services';
+
+const token = {
+  set(token: string) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const LogIn = (formData: any) => async (dispatch: any) => {
+  dispatch(action.logInRequest({}));
+
+  try {
+    const { data } = await httpLogin(formData);
+    token.set(data.token);
+    dispatch(action.logInSuccess(data));
+  } catch (error) {
+    console.log(error);
+    dispatch(action.logInError(error));
+  }
+};
+
+export const signUp = (formData: any) => async (dispatch: any) => {
+  dispatch(action.signUpRequest({}));
+
+  try {
+    const { data } = await httpRegistration(formData);
+    token.set(data.token);
+    dispatch(action.signUpSuccess(data));
+  } catch (error) {
+    console.log(error);
+    dispatch(action.signUpError(error));
+  }
+};
+
+export const logOut = () => async (dispatch: any) => {
+  dispatch(action.logOutRequest({}));
+
+  try {
+    await httpLogOut();
+    token.unset();
+    dispatch(action.logOutSuccess({}));
+  } catch (error) {
+    console.log(error);
+    dispatch(action.logOutError(error));
+  }
+};
+
+// export const getCurrentUser = () => async (dispatch: any, getState: any) => {
+//   dispatch(action.getCurrentUserRequest({}));
+
+//   const state = getState();
+//   const persistedToken = state.auth.token;
+//   console.log('persistedToken', persistedToken);
+//   if (persistedToken === null) {
+//     console.log('Токена нет, уходим из getCurrentUser');
+//     return;
+//   }
+//   token.set(persistedToken);
+//   try {
+//     const { data } = await fetchGetCurrentUser();
+//     console.log('data current user', data);
+//     dispatch(action.getCurrentUserSuccess(data));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
---
+++ src/redux/auth/authSelector.ts
@@ -0,0 +1,3 @@
+export const getUser = (state: any) => state.auth.user;
+
+export const getIsLoggedIn = (state: any) => state.auth.isLoggedIn;
